1561003615a06eac2eed742b003eca06d1b07a88
Aurelien Chaudagne <ouralien@gmail.com>
2015-10-18 18:37:52 -0700

    Displays DateTime in local time.

--- wid-card-process-status.js
+++ wid-card-process-status.js
@@ -52,6 +52,17 @@
         type: String,
         value: '10/21/85 04:29'
       }
+    },
+
+    _localDateTime: function _localDateTime(serverdate) {
+      var date = new Date(serverdate + ' UTC');
+      var month = date.getMonth() + 1;
+      var day = date.getDate();
+      var year = String(date.getFullYear()).substring(2, 4);
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+
+      return month + '/' + day + '/' + year + ' ' + hours + ':' + minutes;
     }
 
   });
